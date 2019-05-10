a1b220536f65389f46b39dd11cc5c970e18fdfe3
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-05-10 23:25:35 +0200

    and another one

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-cd building
+echo "welcome to build.sh"
+echo $PWD
 
+cd ./building
+
+echo "preBuild.sh"
 ./preBuild.sh
 
+echo "generateHTMLSchema.sh"
 ./generateHTMLSchema.sh
+echo "generatePNGSchema.sh"
 ./generatePNGSchema.sh
+echo "linux.sh"
 ./linux.sh
+echo "windows.sh"
 ./windows.sh
 
+echo "postBuild.sh"
 ./postBuild.sh
 
 cd ..
