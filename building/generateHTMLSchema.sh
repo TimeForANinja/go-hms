73718daa8bdde1588cbc84f32d290604a482fa26
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-05-11 02:25:53 +0200

    missing some artifacts

--- building/generateHTMLSchema.sh
+++ building/generateHTMLSchema.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "-- generateHTMLSchema"
-./schemacrawler-15.06.01-distribution/_schemacrawler/schemacrawler.sh -loglevel=CONFIG -server sqlite -database ./test.db -user -password -infolevel standard -command schema -outputformat htmlx -outputfile ../artifacts/sql_schema.html
+./schemacrawler-15.06.01-distribution/_schemacrawler/schemacrawler.sh -server sqlite -database ./test.db -user -password -infolevel standard -command schema -outputformat htmlx -outputfile ../artifacts/sql_schema.html
