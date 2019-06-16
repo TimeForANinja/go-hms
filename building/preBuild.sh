6066cb3365007c5e11acfd6193db23557d370377
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-06-16 19:23:45 +0200

    readd what we just deleted

--- building/preBuild.sh
+++ building/preBuild.sh
@@ -2,9 +2,22 @@
 
 mkdir -p ../artifacts
 
+echo "-- prepare"
+apt update
+
 echo "-- creating database"
 go run ./initDB.go
 
 echo "-- prepare schemacrawler"
 sudo apt install -y graphviz unzip default-jre default-jdk
 unzip schemacrawler-15.06.01-distribution.zip
+
+echo "-- prepare linux build"
+sudo apt install -y libssl-dev build-essential curl git
+
+echo "-- prepare windows build"
+sudo apt install -y mingw-w64
+
+echo "-- install dependencies"
+go get github.com/xeodou/go-sqlcipher
+go get golang.org/x/crypto/bcrypt
