8f6342d608323e83b646d2509141f2c777a41a08
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-06-16 19:43:18 +0200

    update artifacts

--- building/preBuild.sh
+++ building/preBuild.sh
@@ -2,21 +2,21 @@
 
 mkdir -p ../artifacts
 
-echo "-- prepare"
-apt update
+echo "-- prepare apt"
+sudo apt update
 
 echo "-- creating database"
 go run ./initDB.go
 
 echo "-- prepare schemacrawler"
-sudo apt install -y graphviz unzip default-jre default-jdk
-unzip schemacrawler-15.06.01-distribution.zip
+sudo apt install -y graphviz unzip default-jre default-jdk > /dev/null
+unzip schemacrawler-15.06.01-distribution.zip > /dev/null
 
 echo "-- prepare linux build"
-sudo apt install -y libssl-dev build-essential curl git
+sudo apt install -y libssl-dev build-essential curl git > /dev/null
 
 echo "-- prepare windows build"
-sudo apt install -y mingw-w64
+sudo apt install -y mingw-w64 > /dev/null
 
 echo "-- install dependencies"
 go get github.com/xeodou/go-sqlcipher
